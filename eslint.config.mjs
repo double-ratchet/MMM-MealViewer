b11024a765082035c44504c6bcc97910778a22a9
double-ratchet <double-ratchet@pm.me>
2026-01-30 08:05:25 -0500

    refactor node_helper.js to use built-in fetch instead of axios, fix ESLint v9 config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,31 @@
 import js from "@eslint/js";
 import globals from "globals";
+import { defineConfig } from "eslint/config";
 
-export default [
-    js.configs.recommended,
-    {
-        files: ["**/*.js"],
-        languageOptions: {
-            ecmaVersion: 2022,
-            sourceType: "commonjs",
-            globals: {
-                ...globals.browser,
-                ...globals.node,
-                Log: "readonly",
-                Module: "readonly"
-            }
-        },
-        rules: {
-            "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-            "no-console": "off",
-            "indent": ["error", 4],
-            "linebreak-style": ["error", "unix"],
-            "quotes": ["error", "double"],
-            "semi": ["error", "always"]
-        }
+export default defineConfig([
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "commonjs",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        Log: "readonly",
+        Module: "readonly"
+      }
     },
-    {
-        ignores: ["node_modules/"]
+    rules: {
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "no-console": "off",
+      indent: ["error", 4],
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "double"],
+      semi: ["error", "always"]
     }
-];
+  },
+  {
+    ignores: ["node_modules/"]
+  }
+]);
